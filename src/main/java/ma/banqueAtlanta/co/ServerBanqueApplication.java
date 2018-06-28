3bef25bb892cd43d5155ce9f8cfda2c1dbdef5a7
unknown <Administrateur@atlanta_12.atlanta.intra>
2018-06-28 10:10:19 +0000

    first commit

---
+++ src/main/java/ma/banqueAtlanta/co/ServerBanqueApplication.java
@@ -0,0 +1,12 @@
+package ma.banqueAtlanta.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBanqueApplication.class, args);
+	}
+}
